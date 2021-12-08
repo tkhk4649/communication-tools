aa3f639476afad1901edad7c2cd9f9bf17d62ac6
ebifly144 <95830418+ebifly144@users.noreply.github.com>
2021-12-09 08:40:16 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    if (mode == 0) {
+        mode = 1
+        radio.sendValue("mode", 1)
+        basic.showString("Sending")
+    }
+})
+radio.onReceivedString(function (receivedString) {
+	
+})
+let mode = 0
+radio.setGroup(1)
+basic.forever(function () {
+	
+})
